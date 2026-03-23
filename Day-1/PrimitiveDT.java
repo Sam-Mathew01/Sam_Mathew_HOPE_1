a5b89432d805fe0b34135556399ba6e68ca4dda9
SAM MATHEW S A <samjomathew36@gmail.com>
2026-03-23 12:55:39 +0530

    Add files via upload

---
+++ Day-1/PrimitiveDT.java
@@ -0,0 +1,23 @@
+public class PrimitiveDT {
+    public static void main(String[] args) {
+        byte a=-128;
+        byte b=127;
+        System.out.println("The Range of byte is:"+a+","+b);
+        short sh1=-32768;
+        short sh2=32767;
+        System.out.println("The Range of short is:"+sh1+","+sh2);
+        int num1=-2147483648;
+        int num2=2147483647;
+        System.out.println("The Range of int is:"+num1+","+num2);
+        long l1=250L;
+        System.out.println(l1);
+        boolean bool1=true;
+        boolean bool2=false;
+        System.out.println("Values of Boolean is:"+bool1+","+bool2);
+        float f=10.5f;
+        System.out.println(f);
+        double d=200000.2599;
+        System.out.println(d);
+
+    }
+}
